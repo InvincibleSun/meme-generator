387a863d59ac5031be8ea3f7f620623a843e853a
InvincibleSun <103495967+InvincibleSun@users.noreply.github.com>
2022-11-28 21:15:12 +0400

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="App-header">
+      <img src="" className="App-logo" alt="logo" />
+      <p></p>
+    </header>
+  );
+}
+
+export default Header;
